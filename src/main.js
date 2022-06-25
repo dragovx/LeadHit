98e0ab8093d9c809c5b6804aa066428fba81e2cd
dragovx <dragovx@mail.ru>
2022-06-26 00:19:04 +0300

    LeadHit

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import 'materialize-css/dist/js/materialize.min'
+import Vuex from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(Vuex).mount('#app')
